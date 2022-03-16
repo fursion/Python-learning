e9625ff21c80997b6ecc4d5a6f17b7b676579718
洁 杜 <604357968@qq.com>
2022-03-17 00:03:34 +0800

    update

--- Project_1/main.py
+++ Project_1/main.py
@@ -1,13 +1,16 @@
 import time
 import datetime
-from Project_2 import ceshi
+#from Project_2 import ceshi
 
 from datetime import datetime as f_datetime
 # 查看时间
 now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print(now)
-
-
+class Testclass(object):
+    def __init__(self) -> None:
+        self.age = 30
+    def fun01():
+        print("Testclass_Fun01")
 def fun1():
     print(time.time())
     print(f_datetime.today())
@@ -16,13 +19,7 @@ def fun1():
     print(time.time())
     print(type(da))
     test = Testclass()
-    test.fun01()
+    # test.fun01()
     print(f"age :{test.age}")
-    ceshi.im()
-
-class Testclass(object):
-    def __init__(self) -> None:
-        self.age = 30
-    def fun01():
-        print("Testclass_Fun01")
+    #ceshi.im() 
 fun1()
--- Project_2/ceshi.py
+++ Project_2/ceshi.py
@@ -1,2 +1,15 @@
 def im():
     print("im test")
+
+
+class C_a:
+    def __init__(self) -> None:
+        print("C_a")
+
+
+class C_b(C_a):
+    def __init__(self) -> None:
+        print("C_b")
+    def fun01():
+        print("C_b_fun01")
+c=C_b()
